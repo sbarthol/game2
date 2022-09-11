d8b4132a3a09ca468c004473ce66d7022c76e655
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-09-11 16:32:52 -0400

    first draft of the maze

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -26,15 +26,6 @@ struct PlayMode : Mode {
 
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
-
-	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
 	
 	//camera:
 	Scene::Camera *camera = nullptr;
